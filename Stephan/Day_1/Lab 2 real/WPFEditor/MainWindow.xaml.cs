99c37190b2ccdc5017d11e617439f2bff48c621a
stephko <s.koerber93@gmail.com>
2018-09-05 16:24:27 +0200

    day_1

---
+++ Stephan/Day_1/Lab 2 real/WPFEditor/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void onOpen(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            bool? result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                string text = File.ReadAllText(filename);
+                TextEditor.Text = text;
+            }
+        }
+    }
+}
---
+++ Stephan/Day_1/Lab 2/BookSample/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSample.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+
+
+        }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public string Isbn { get; set; }
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
