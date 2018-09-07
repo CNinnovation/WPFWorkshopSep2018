2a17a0795ea67a306675806b33eec8279e200bc1
Christian Nagel <christian@christiannagel.com>
2018-09-07 11:13:30 +0200

    validation sample

---
+++ Day2/ValidationSample/ValidationSample/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ValidationSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SomeData _someData = new SomeData { X1 = 2, X2 = 2 };
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = _someData;
+        }
+
+        private void OnShow(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"x1: {_someData.X1}, x2: {_someData.X2}");
+        }
+    }
+}
---
+++ Day2/ValidationSample/ValidationSample/SomeData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ValidationSample
+{
+    public class SomeData : IDataErrorInfo
+    {
+        private int _x1;
+
+        public int X1
+        {
+            get { return _x1; }
+            set
+            {
+                if (value > 50) throw new Exception("bad value");
+                _x1 = value;
+            }
+        }
+
+        public int X2 { get; set; }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string result = null;
+                switch (columnName)
+                {
+                    case "X2":
+                        if (X2 > 50) result = "wrong value with X2";
+                        break;
+                    default:
+                        break;
+                }
+                return result;
+            }
+        }
+
+
+
+        public string Error => throw new NotImplementedException();
+    }
+}
