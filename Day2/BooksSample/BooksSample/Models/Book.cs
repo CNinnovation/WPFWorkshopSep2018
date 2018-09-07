945ee6e48d00d355b716692914ce21a24300c914
Christian Nagel <christian@christiannagel.com>
2018-09-07 08:15:19 +0200

    kick-off day 2 with sample from day 1

---
+++ Day2/BooksSample/BooksSample/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksSample.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+
+        }
+        public int BookdId { get; set; }
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public string[] Authors { get; set; }
+        public override string ToString() => Title;
+    }
+}
---
+++ Day2/BooksSample/BooksSample/Models/BooksFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksSample.Models
+{
+    public class BooksFactory
+    {
+        public Book GetTheBook() => new Book { BookdId = 1, Isbn = "3488934", Title = "Professional C# 8", Publisher = "Wrox Press", Authors = new string[] { "Christian Nagel" } };
+
+    }
+}
